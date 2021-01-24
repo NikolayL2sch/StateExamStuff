c313442e91ab2883c101ec391a8910e619485422
Nikolay Milenko <31999179+NikolayL2sch@users.noreply.github.com>
2021-01-24 17:55:56 +0300

    Update ege.py

--- ege.py
+++ ege.py
@@ -15,12 +15,10 @@ def rec(n,k):
         if k == 6 and n not in ans: # проверяем кол-во проделанных команд
             ans.append(n)  
             return 1
-        else:
-            return 0
     k += 1
     return rec(n+1,k)+rec(n+2,k)+rec(n*2,k)
  
-print(rec(n,k))
-print(*ans)
+rec(n,k)
+print(len(ans))
 t1 = time.time() - t0
 #print("Time elapsed: ", t1)
