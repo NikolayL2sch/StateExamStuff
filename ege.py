f392a0bf2bb8b341c18019cecfb63a1210533e7e
Nikolay Milenko <31999179+NikolayL2sch@users.noreply.github.com>
2021-04-07 22:10:45 +0300

    Add files via upload

--- ege.py
+++ ege.py
@@ -1,24 +1,13 @@
-import time
-t0 = time.time()
-n = 1
-k = 0
-ans = []
-def rec(n,k):
-    global left, right #глобальные границы отрезка
-    left = 34
-    right = 59
-    if k > 6:
-        return 0
-    if n > right:  # выход за границу справа
-        return 0
-    if left <= n <= right:  # все ок
-        if k == 6 and n not in ans: # проверяем кол-во проделанных команд
-            ans.append(n)  
-            return 1
-    k += 1
-    return rec(n+1,k)+rec(n+2,k)+rec(n*2,k)
- 
-print(rec(n,k))
-print(*ans)
-t1 = time.time() - t0
-#print("Time elapsed: ", t1)
+t = []
+m = 0
+s = '0'
+n = int(input())
+for i in range (n):
+    t.append(int(input()))
+for i in range (n):
+    for j in range (n):
+        if i!=j and abs(t[i]-t[j])%2 == 0 and (t[i] % 11 == 0 or t[j] % 11 == 0):
+            if t[i]*t[j] > m:
+                m = t[i]*t[j]
+                s = str(min(t[j],t[i]))+str(max(t[j],t[i]))
+print(s,m)
